39d6b7bf4f3bb3c2c7f0d65a7a5d51bda084738b
LuckyZacha <zachatatem@gmail.com>
2022-06-20 08:21:26 +0800

    fix code for merge resourceRestful api

--- Paper/views/ResourceRestful.py
+++ Paper/views/ResourceRestful.py
@@ -1,5 +1,7 @@
 import django.db.utils
 from django.http import JsonResponse
+from rest_framework.decorators import action
+
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets, status

9062329eeccf0d52538a22278efc53415f25966c
LuckyZacha <zachatatem@gmail.com>
2022-06-25 10:10:53 +0800

    Fix: when fetch, return paginated data

--- Paper/views/ResourceRestful.py
+++ Paper/views/ResourceRestful.py
@@ -158,12 +158,14 @@ class ResourceViewSet(viewsets.ModelViewSet):
     def fetch(self, request):
         try:
             serializer = ResourceDetailSerializer(self.queryset, many=True)
-            
-            return JsonResponse({
-                'response_code': True,
-                'data': serializer.data,
-                'message': 'Fetch detailed resource succeed'
-            })
+            queryset = self.filter_queryset(self.get_queryset())
+            page = self.paginate_queryset(queryset)
+            if page is not None:
+                serializer = self.get_serializer(page, many=True)
+                return self.get_paginated_response(serializer.data)
+
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data)
 
         except Exception as e:
             print(e)
